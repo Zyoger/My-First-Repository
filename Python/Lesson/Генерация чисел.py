0eb09b471b9637382e1a4eb836caf4be60b8f275
Zyoger <zyoger@googlemail.com>
2022-01-07 12:51:00 +0300

    go

---
+++ Python/Lesson/Генерация чисел.py
@@ -0,0 +1,12 @@
+def genegate_numbers(N:int, M:int, prefix=None):
+    if M == 0:
+        print(prefix)
+        return
+    prefix = prefix or []
+    for digit in range(N):
+        prefix.append(digit)
+        genegate_numbers(N, M-1, prefix)
+        prefix.pop()
+
+
+genegate_numbers(11, 2)
