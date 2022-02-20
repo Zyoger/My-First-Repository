04ff9bf818978eb89d6d83493c0e892aefef1a7b
zyoge <zyoger@gmail.com>
2022-02-20 22:38:51 +0300

    Update 20.02.2022

--- Python/UDEMI lesson/52/2.py
+++ Python/UDEMI lesson/52/2.py
@@ -1,6 +1,7 @@
 """Определить класс “Матрица” со свойствами: размерность матрицы, элементы матрицы, - и методами:
 вывод матрицы на экран, вычисление определителя матрицы (для матриц с размерностью 2 или 3) и проверки,
 является ли матрица единичной, нулевой или диагональной.
+
 Измените класс “Матрица” следующим образом:
 свойства класса - размерность матрицы и элементы - должны быть динамическими и задаваться внутри нового метода,
 определяющего матрицу;
@@ -11,19 +12,48 @@
 
 
 class matrix():
-    array = [[1, 0, 0], [0, 2, 0], [0, 0, 3]]
-    N = 3
-    M = 3
+    # статические переменные
+    numbermatrix = 0
+    diagmatrix = 0
+    nulmatrix = 0
+    edmatrix = 0
+
+    def set(self, n, m, array):
+        """
+        Задает динамические параметры прямой.
+        :return:
+        """
+        self.n = n
+        self.m = m
+        self.arr = array
+        matrix.numbermatrix += 1
 
     def print(self):
-        print("MATRIX:")
-        for i in range(0, len(self.array)):
-            for j in range(0, len(self.array[i])):
-                print(self.array[i][j], end=' | ')
+        """
+         Выводит матрицу на экран.
+        """
+        self.diagonalmatrix()
+        self.nullmatrix()
+        self.identitymatrix()
+        print("*"*60)
+        print("Количество созданых матриц: ", matrix.numbermatrix)
+        print("Количество диагональных матриц: ", matrix.diagmatrix)
+        print("Количество нулевых матриц: ", matrix.nulmatrix)
+        print("Количество еденичных матриц: ", matrix.edmatrix)
+        print(f"MATRIX {matrix.numbermatrix}:")
+        arr = self.arr
+        for i in range(0, len(arr)):
+            for j in range(0, len(arr[i])):
+                print(arr[i][j], end=' | ')
             print()
+        self.determinant()
 
     def determinant(self):
-        arr = self.array
+        """
+        Расчитывает определитель матрицы.
+        :return: определитель
+        """
+        arr = self.arr
         if len(arr) == 2:
             determatrix = arr[0][0]*arr[1][1] - arr[1][0]*arr[0][1]
             print("Данная матрица имеет размерность 2х2.")
@@ -31,12 +61,17 @@ class matrix():
         elif len(arr) == 3:
             determatrix = arr[0][0]*arr[1][1]*arr[2][2] + arr[0][1]*arr[1][2]*arr[2][0] + arr[0][2]*arr[1][0]*arr[2][1]\
                           - arr[0][2]*arr[1][1]*arr[2][0] - arr[0][0]*arr[1][2]*arr[2][1] - arr[0][1]*arr[1][0]*arr[2][2]
+            print("Данная матрица имеет размерность 3х3.")
             print(f"Определитель для матрицы равен {determatrix}")
         else:
             print("Параметры данной матрицы не подходят для определения определителя")
 
     def identitymatrix(self):
-        arr = self.array
+        """
+        Функция определяет является ли матрица единичной.
+        :return: True если матрица единичная.
+        """
+        arr = self.arr
         flag = True
         for i in range(0, len(arr)):
             for j in range(0, len(arr[i])):
@@ -46,19 +81,31 @@ class matrix():
                 if i != j:
                     if arr[i][j] != 0:
                         flag = False
+        if flag:
+            matrix.edmatrix += 1
         return flag
 
     def nullmatrix(self):
-        arr = self.array
+        """
+        Функция определяет является ли матрица нулевой.
+        :return: True если матрица нулевая.
+        """
+        arr = self.arr
         flag = True
         for i in range(0, len(arr)):
             for j in range(0, len(arr[i])):
                 if arr[i][j] != 0:
                     flag = False
+        if flag:
+            matrix.nulmatrix += 1
         return flag
 
     def diagonalmatrix(self):
-        arr = self.array
+        """
+        Функция определяет является ли матрица диагональной.
+        :return: True если матрица диагональная.
+        """
+        arr = self.arr
         flag = True
         for i in range(0, len(arr)):
             for j in range(0, len(arr[i])):
@@ -68,14 +115,26 @@ class matrix():
                 if i != j:
                     if arr[i][j] != 0:
                         flag = False
+        if flag:
+            matrix.diagmatrix += 1
         return flag
 
     def propertiesmatrix(self):
         pass
 
 
-matrix().print()
-matrix().determinant()
-matrix().identitymatrix()
-matrix().nullmatrix()
-matrix().diagonalmatrix()
+arr1 = [[1, 0, 0], [0, 2, 0], [0, 0, 3]]
+m1 = matrix()
+m1.set(3, 3, arr1)
+m1.print()
+
+arr2 = [[0, 0], [0, 0]]
+m2 = matrix()
+m2.set(2, 2, arr2)
+m2.print()
+
+arr3 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+m3 = matrix()
+m3.set(3, 3, arr3)
+m3.print()
+# print(matrix().numbermatrix)
